52539cb39c015252fc853ce40a870225b4ac5757
PythonGuyVolg <63300530+PythonGuyVolg@users.noreply.github.com>
2020-04-07 19:44:25 +0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from EgorEnginev1t1 import *
 import random
 '''Module code is end'''
 musicFile1 = 'EarthSound.mp3'
-musicFile2= 'Doom-E1M1_SC-8850.mp3.mp3'
+musicFile2= 'Doom-E1M1_SC-8850.mmp3'
 arr = [['1','0','0','0','0','0','0','0','0','0'],
       ['0','1','1','0','0','0','0','0','1','1'],
       ['0','0', '0','1','0','1','1','1','0','0', '1'],
